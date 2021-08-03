10db7fcab76303bf6d9eae0e93aa7c5209f3e7be
Eydie <you@example.com>
2021-08-03 14:41:06 -0500

    Committing initial steps for menu and menu item - created as a package

---
+++ launchcode/Java/Practice Space/src/ControlFlowCollections.java
@@ -0,0 +1,8 @@
+public class ControlFlowCollections {
+
+    public static void main(String [] args) {
+        do {
+            System.out.println("Hello world!");
+        } while (3 < 2);
+    }
+}
