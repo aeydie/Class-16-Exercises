9b2a4daa03911a43e7767b69649ccf2e55b5e01a
Eydie <you@example.com>
2021-08-03 14:33:18 -0500

    Committing initial steps for menu and menu item - created as a package

---
+++ launchcode/Java/Menu Package/src/Studio3/Menu.java
@@ -0,0 +1,27 @@
+package Studio3;
+
+import java.util.Date;
+import java.util.MenuItem;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Menu {
+    public static void main (String[] args) {
+
+        private String name;
+        private String address;
+        private String hours;
+        private String phoneNumber;
+        private String dateLastUpdated;
+
+
+        public Menu(String name, String address, String hours, String phoneNumber, String dateLastUpdated){
+        }
+            this.name = name;
+            this.address = address;
+            this.hours = hours;
+            this.phoneNumber = phoneNumber;
+            this.dateLastUpdated = dateLastUpdated;
+    }
+}
