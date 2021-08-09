ab04613de53631f298ad1023cc9bf6ae23345ce6
Eydie <you@example.com>
2021-08-09 11:48:50 -0500

    Committing studio 4 java second time Restaurant class is red still

--- launchcode/Java/Menu and Restaurant/src/Restaurant/Menu.java
+++ launchcode/Java/Menu and Restaurant/src/Restaurant/Menu.java
@@ -1,73 +1,76 @@
 package Restaurant;
 
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Menu {
 
-    private ArrayList<MenuItem> allMenuItems = new ArrayList<>();
-//    private LocalDate lastupdated;
+    //fields
+    private static ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private LocalDate lastUpdated;
 
-    public ArrayList<MenuItem> getAllMenuItems() {
-        return allMenuItems;
+    //getter and setter
+    public static ArrayList<MenuItem> getMenuItem() {
+        return menuItems;
     }
 
-    public void setAllMenuItems(ArrayList<MenuItem> allMenuItems) {
-        this.allMenuItems = allMenuItems;
+    public LocalDate getLastUpdated() {
+        return lastUpdated;
     }
 
-    public ArrayList<MenuItem> printAllMenuItems() {
-        return allMenuItems;
+    public void setLastUpdated(LocalDate lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    //methods
+    public static ArrayList<MenuItem> printAllMenuItems() {
+        return menuItems;
+    }
+
+    public static MenuItem printMenuItem(MenuItem itemToBePrinted) {
+        return itemToBePrinted;
     }
 
     public static void addMenuItem(MenuItem itemToBeAdded) {
-        allMenuItems.add(itemToBeAdded);
+        menuItems.add(itemToBeAdded);
     }
 
-
+    public static void removeMenuItem(MenuItem itemToBeRemoved) {
+        menuItems.remove(itemToBeRemoved);
+    }
 }
 
+//    public static MenuItem lastUpdated(MenuItem itemToBeDated) {
+//        return itemToBeDated.dateCreated;
+//    }
+//
+//
+//
+//}
 //import java.time.LocalDate;
-//        import java.util.ArrayList;
+//import java.util.ArrayList;
 //
 //public class Menu {
 //
-//    //fields
-//    private static ArrayList<MenuItem> menuItems = new ArrayList<>();
-//    private LocalDate lastUpdated;
+//    private ArrayList<MenuItem> allMenuItems = new ArrayList<>();
+////    private LocalDate lastupdated;
 //
-//    //getter and setter
-//    public static ArrayList<MenuItem> getMenuItem() { return menuItems; }
-//
-//    public LocalDate getLastUpdated() {
-//        return lastUpdated;
+//    public ArrayList<MenuItem> getAllMenuItems() {
+//        return allMenuItems;
 //    }
 //
-//    public void setLastUpdated(LocalDate lastUpdated) {
-//        this.lastUpdated = lastUpdated;
+//    public void setAllMenuItems(ArrayList<MenuItem> allMenuItems) {
+//        this.allMenuItems = allMenuItems;
 //    }
 //
-//    //methods
-//    public static ArrayList<MenuItem> printAllMenuItems() {
-//        return menuItems;
-//    }
-//
-//    public static MenuItem printMenuItem(MenuItem itemToBePrinted) {
-//        return itemToBePrinted;
+//    public ArrayList<MenuItem> printAllMenuItems() {
+//        return allMenuItems;
 //    }
 //
 //    public static void addMenuItem(MenuItem itemToBeAdded) {
-//        menuItems.add(itemToBeAdded);
+//        allMenuItems.add(itemToBeAdded);
 //    }
-//
-//    public static void removeMenuItem(MenuItem itemToBeRemoved) {
-//        menuItems.remove(itemToBeRemoved);
-//    }
-//
-////    public static MenuItem lastUpdated(MenuItem itemToBeDated) {
-////        return itemToBeDated.dateCreated;
-////    }
-//
 //
 //
 //}
--- launchcode/Java/Menu and Restaurant/src/Restaurant/MenuItem.java
+++ launchcode/Java/Menu and Restaurant/src/Restaurant/MenuItem.java
@@ -1,63 +1,138 @@
+
 package Restaurant;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Objects;
-import java.util.Arrays;
-import java.util.ArrayList;
 
-public class MenuItem {
+        public class MenuItem {
 
-        private String name;
-        private double price;
-        private String description;
-        private String category;
-        private boolean isNew;
 
-        public String getName() {
-        return name;
-        }
-        public void setName(String name) {
-        this.name = name;
-        }
-        public double getPrice() {
-        return price;
-        }
-        public void setPrice(double price) {
-        this.price = price;
-        }
+                //fields
+                private String name;
+                private double price;
+                private String description;
+                private String category;
+                private boolean isNew;
 
-        public String getDescription() {
-        return description;
-        }
-        public void setDescription(String description) {
-        this.description = description;
-        }
+                //protected int dateCreated = new LocalDate.now();
 
-        public String getCategory() {
-        return category;
-        }
-        public void setCategory(String category) {
-        this.category = category;
-        }
+                //constructor
+                public MenuItem(String name, double price, String description, String category, boolean isNew) {
+                        this.name = name;
+                        this.price = price;
+                        this.description = description;
+                        this.category = category;
+                        this.isNew = isNew;
+                }
 
-        public boolean getIsNew() { return isNew; }
-//                public void setNew(boolean isNew) { this.isNew = isNew; }
 
-//        public LocalDate getLastUpdated() {
-//        return lastUpdated;
+                //getters and setters
+                public String getName() { return name; }
+                public void setName(String name) { this.name = name; }
+
+                public  double getPrice() { return price; }
+                public void setPrice(double price) { this.price = price; }
+
+                public String getDescription() { return description; }
+                public void setDescription(String description) { this.description = description; }
+
+                public String getCategory() { return category; }
+                public void setCategory(String category) { this.category = category; }
+
+                public boolean getIsNew() { return isNew; }
+                public void setNew(boolean isNew) { this.isNew = isNew; }
+
+//    public int getDateCreated() {
+//        return dateCreated;
+//    }
+//    public void setDateCreated(int dateCreated) {
+//        this.dateCreated = dateCreated;
+//    }
+
+                //ToString method
+                public String toString() {
+                        return "*****\n" + "Name: " + this.name.toUpperCase() + "\n" + this.description.toUpperCase() + "\n" + "Price: " + this.price + "\n*****";
+                }
+
+                //Equals Method
+                @Override
+                public boolean equals(Object o) {
+                        if (this == o) return true;
+                        if (!(o instanceof MenuItem)) return false;
+                        MenuItem menuItem = (MenuItem) o;
+                        return Double.compare(menuItem.price, price) == 0 && isNew == menuItem.isNew && Objects.equals(name, menuItem.name) && Objects.equals(description, menuItem.description) && Objects.equals(category, menuItem.category);
+                }
+
+                @Override
+                public int hashCode() {
+                        return Objects.hash(name, price, description, category, isNew);
+                }
+
+
+        }
+//package Restaurant;
+//
+//import java.time.LocalDate;
+//import java.util.Date;
+//import java.util.Objects;
+//import java.util.Arrays;
+//import java.util.ArrayList;
+//
+//public class MenuItem {
+//
+//        private String name;
+//        private double price;
+//        private String description;
+//        private String category;
+//        private boolean isNew;
+//
+//        public String getName() {
+//        return name;
 //        }
-//        public void setLastUpdated(LocalDate lastUpdated) {
-//        this.lastUpdated = lastUpdated;
+//        public void setName(String name) {
+//        this.name = name;
 //        }
-
-        public MenuItem(String name, double price, String description, String category, boolean isNew) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.category = category;
-        this.isNew = isNew;
-    }
+//        public double getPrice() {
+//        return price;
+//        }
+//        public void setPrice(double price) {
+//        this.price = price;
+//        }
+//
+//        public String getDescription() {
+//        return description;
+//        }
+//        public void setDescription(String description) {
+//        this.description = description;
+//        }
+//
+//        public String getCategory() {
+//        return category;
+//        }
+//        public void setCategory(String category) {
+//        this.category = category;
+//        }
+//
+//        public boolean getIsNew() { return isNew; }
+////                public void setNew(boolean isNew) { this.isNew = isNew; }
+//
+////        public LocalDate getLastUpdated() {
+////        return lastUpdated;
+////        }
+////        public void setLastUpdated(LocalDate lastUpdated) {
+////        this.lastUpdated = lastUpdated;
+////        }
+//
+//        public MenuItem(String name, double price, String description, String category, boolean isNew) {
+//        this.name = name;
+//        this.price = price;
+//        this.description = description;
+//        this.category = category;
+//        this.isNew = isNew;
+//    }
 
 //    @Override
 //    public String toString() {
@@ -92,79 +167,6 @@ public class MenuItem {
 
 //        public void setPrice (Double userPrice){
 //            this.price = userPrice;
-//        }
-
-//import java.time.LocalDate;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.Date;
-//import java.util.Objects;
-//
-//        public class MenuItem {
-//
-//
-//                //fields
-//                private String name;
-//                private double price;
-//                private String description;
-//                private String category;
-//                private boolean isNew;
-//
-//                //protected int dateCreated = new LocalDate.now();
-//
-//                //constructor
-//                public MenuItem(String name, double price, String description, String category, boolean isNew) {
-//                        this.name = name;
-//                        this.price = price;
-//                        this.description = description;
-//                        this.category = category;
-//                        this.isNew = isNew;
-//                }
-//
-//
-//                //getters and setters
-//                public String getName() { return name; }
-//                public void setName(String name) { this.name = name; }
-//
-//                public  double getPrice() { return price; }
-//                public void setPrice(double price) { this.price = price; }
-//
-//                public String getDescription() { return description; }
-//                public void setDescription(String description) { this.description = description; }
-//
-//                public String getCategory() { return category; }
-//                public void setCategory(String category) { this.category = category; }
-//
-//                public boolean getIsNew() { return isNew; }
-//                public void setNew(boolean isNew) { this.isNew = isNew; }
-//
-////    public int getDateCreated() {
-////        return dateCreated;
-////    }
-////    public void setDateCreated(int dateCreated) {
-////        this.dateCreated = dateCreated;
-////    }
-//
-//                //ToString method
-//                public String toString() {
-//                        return "*****\n" + "Name: " + this.name.toUpperCase() + "\n" + this.description.toUpperCase() + "\n" + "Price: " + this.price + "\n*****";
-//                }
-//
-//                //Equals Method
-//                @Override
-//                public boolean equals(Object o) {
-//                        if (this == o) return true;
-//                        if (!(o instanceof MenuItem)) return false;
-//                        MenuItem menuItem = (MenuItem) o;
-//                        return Double.compare(menuItem.price, price) == 0 && isNew == menuItem.isNew && Objects.equals(name, menuItem.name) && Objects.equals(description, menuItem.description) && Objects.equals(category, menuItem.category);
-//                }
-//
-//                @Override
-//                public int hashCode() {
-//                        return Objects.hash(name, price, description, category, isNew);
-//                }
-//
-//
 //        }
 
 
