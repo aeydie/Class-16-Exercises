aa63e22227a7065799d88e0df78aee7625ac3efd
Eydie <you@example.com>
2021-08-31 14:27:45 -0500

    tracking progress for Springboot practice now with forms

--- Springboot/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ Springboot/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,23 +1,49 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestBody
 public class HelloController {
 
     //lives at the path /hello
-    @GetMapping("hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
 
+    //lives at /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
+
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello"   )
+    //lives at /hello?name=launchcode
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello " + name + "!";
+    }
+ //handler that handles request of the form /hello/launchcode
+
+    @GetMapping("hello/{name}")
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello " + name + "!";
+    }
+
+    @GetMapping("form")
+    @ResponseBody
+    public String helloFrom() {
+        return "<html>" +
+                "<body>" +
+                "<form action= 'hello' method= 'post'>" +
+                "<input type= 'text' name= 'name'>" +
+                "<input type= 'submit' value= 'Greet me'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
 }
