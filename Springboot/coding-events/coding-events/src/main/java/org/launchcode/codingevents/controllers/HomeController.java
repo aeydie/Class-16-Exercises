212b94ec4ef3618fc233932a8961591bfaa2ed12
Eydie <you@example.com>
2021-09-07 23:50:38 -0500

    second commit 11.6

--- Springboot/coding-events/coding-events/src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ Springboot/coding-events/coding-events/src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package org.launchcode.codingevents.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+
 }
