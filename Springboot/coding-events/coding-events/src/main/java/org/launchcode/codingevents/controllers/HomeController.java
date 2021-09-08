8160f676ecda755952805d785756f64d710efc33
Eydie <you@example.com>
2021-09-08 14:36:47 -0500

    third committ just made the form template "create"

--- Springboot/coding-events/coding-events/src/main/java/org/launchcode/codingevents/controllers/HomeController.java
+++ Springboot/coding-events/coding-events/src/main/java/org/launchcode/codingevents/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package org.launchcode.codingevents.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 
     @GetMapping
